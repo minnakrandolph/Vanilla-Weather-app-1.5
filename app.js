3721db66c99d360c8333fee444bcd547120102dd
minna <minnakrandolph@gmail.com>
2023-04-01 12:24:08 -0400

    Update app.js
    added my apiKey and ApiUrl. Deleted alert

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-alert("hello world");
+let apiKey = "22ffdfb8d8d8b00d2858bb969032d4f11";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
+q=New York&appid=${apiKey}`;
+
+console.log(apiUrl);
