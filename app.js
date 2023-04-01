f0a255b43cacd521122f1c21e53295fd81289c19
minna <minnakrandolph@gmail.com>
2023-04-01 16:07:09 -0400

    Update app.js
    declare function, declare axios to display temperature in the console

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+function displayTemperature(response) {
+    console.log(response.data);
+}
+
+
 let apiKey = "22ffdfb8d8d8b00d2858bb969032d4f11";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
 q=New York&appid=${apiKey}units=metric`;
 
 console.log(apiUrl);
+axios.get(url).then(displayTemperature);
