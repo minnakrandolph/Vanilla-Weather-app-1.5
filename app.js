b2f72f236c4653cc860400e1c204802453232283
minna <minnakrandolph@gmail.com>
2023-04-01 16:28:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 function displayTemperature(response) {
     console.log(response.data.main.temp);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = response.data.main.temp;
 }
 
 
 let apiKey = "22ffdfb8d8d8b00d2858bb969032d4f11";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
-q=New York&appid=${apiKey}units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}units=metric`;
 
 console.log(apiUrl);
 axios.get(url).then(displayTemperature);
