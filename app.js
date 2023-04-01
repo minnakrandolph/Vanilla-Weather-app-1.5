25f31ddafbf1d47feba6854bddb8fdcc7754777c
minna <minnakrandolph@gmail.com>
2023-04-01 12:30:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let apiKey = "22ffdfb8d8d8b00d2858bb969032d4f11";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
-q=New York&appid=${apiKey}`;
+q=New York&appid=${apiKey}units=metric`;
 
 console.log(apiUrl);
