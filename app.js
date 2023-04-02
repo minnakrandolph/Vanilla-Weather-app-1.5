fbee0293262434a82b7e2fa5c399adf1544c674e
minna <minnakrandolph@gmail.com>
2023-04-02 08:26:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function displayTemperature(response) {
 
 let apiKey = "2e3dto1e48d1a435aab54b3f664a20b0";
 let city = "New York";
-let apiUrl =  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(url).then(displayTemperature);
 
