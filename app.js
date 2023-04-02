72eb3f0007c9f58e5b038a5a6c3cab382176c2e6
minna <minnakrandolph@gmail.com>
2023-04-02 08:48:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
-    let description = document.querySelector("#city");
+    let description = document.querySelector("#description");
     temperatureElement.innerHTML = Math.round
     (response.data.temperature.current);
     cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.name;
   }
   
   let apiKey = "2e3dto1e48d1a435aab54b3f664a20b0";
-  let city = "New York";
+  //let city = "New York";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 
