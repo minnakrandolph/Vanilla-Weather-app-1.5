a73dbd7e540da6c565822d4adad35544f098fa58
minna <minnakrandolph@gmail.com>
2023-04-02 17:28:23 -0400

    created new formatDate function to show date, day, hours, minutes

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
+function formatDate(timestamp); {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let day = date.getDay();
+    return `${day} ${hours}:${minutes}`;
+}
+
+
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement =  document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
- }
+    dateElement.innerHTML = formatDate(response.data.new Date);
+}
 console.log(temperature);
   
   let apiKey = "2e3dto1e48d1a435aab54b3f664a20b0";
