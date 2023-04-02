5dcb2e4aa9688865f167eb180f9d9ecdc8299ed5
minna <minnakrandolph@gmail.com>
2023-04-02 17:01:24 -0400

    added the following elements:
    city, description(weather) humidity, wind.

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ function displayTemperature(response) {
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement =  document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = response.data.wind.speed;
  }
 console.log(temperature);
   
