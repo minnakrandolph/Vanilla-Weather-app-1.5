b9e87f71fd9ff4533edc961bfc09c45153d3092b
minna <minnakrandolph@gmail.com>
2023-04-03 06:54:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -31,19 +31,20 @@ function displayTemperature(response) {
 }
 console.log(temperature);
 
+function search(city) {
+    let apiKey = "2e3dto1e48d1a435aab54b3f664a20b0";
+    let city = "New York";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    
+    axios.get(apiUrl).then(displayTemperature);
+  }
+
 function handleSubmit(event) {
     city.preventDefault();
     let cityInputElement = document.querySelector("#city-input");
     console.log(cityInputElement.value);
 }
+search("New York");
   
-function search(city) {
-  let apiKey = "2e3dto1e48d1a435aab54b3f664a20b0";
-  let city = "New York";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  
-  axios.get(apiUrl).then(displayTemperature);
-}
-
   let form = document.querySelector("#search-form");
   form.addEventListener("submit", handleSubmit);
