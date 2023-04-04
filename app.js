2f43b373b3d616bf3b0bd6bb659fd9420b10f282
minna <minnakrandolph@gmail.com>
2023-04-04 06:57:02 -0400

    update JS
    created a function to display the celsius temperature alongside fahrenheit links to effect the displayed temperature

--- app.js
+++ app.js
@@ -50,8 +50,12 @@ function displayFahrenheitTemperature(event) {
     let temperatureElement = document.querySelector("#temperature");
     let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
     //alert(fahrenheitTemperature);
-    //let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
   let celsiusTemperature = null;
@@ -61,5 +65,9 @@ function displayFahrenheitTemperature(event) {
 
   let fahrenheitLink = document.querySelector("#fahrenheit-link");
   fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+  let celsiusLink = document.querySelector("#celsius-link");
+  celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 
   search("New York");
