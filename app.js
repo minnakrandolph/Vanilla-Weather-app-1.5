3f1922b4946473e356f18fb9f3ce425c66fc7680
minna <minnakrandolph@gmail.com>
2023-04-08 18:08:42 -0400

    Update app.js
    added days to forecast

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function displayForecast() {
     let forecastElement = document.querySelector("#forecast");
    
     let forecastHTML = `<div class="row">`;
-    let days = ["Thu", "Fri", "Sat"];
+    let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tues", "Wed"];
     days.forEach(function(day) {
     forecastHTML = forecastHTML + 
       `
