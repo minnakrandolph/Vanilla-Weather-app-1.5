73703c3e218fb2c0a7fb39c5d27fa30f6f80dbd4
minna <minnakrandolph@gmail.com>
2023-04-09 18:06:27 -0400

    Update app.js
    updated apiKey

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ function displayTemperature(response) {
 console.log(temperature);
 
 function search(city) {
-    let apiKey = "2e3dto1e48d1a435aab54b3f664a20b0";
+    let apiKey = "597c40c39084687093b091cd48b366f8";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     
     axios.get(apiUrl).then(displayTemperature);
