85d2aa5c8d009b348e732e75b86783c94353b9ce
minna <minnakrandolph@gmail.com>
2023-04-13 08:03:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function displayForecast(response) {
     console.log(coordinates);
     let apiKey = "aa09763d916df0424c840d55bfc2d2c9";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
-    lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=${apiKey}&units=metric`;
     console.log(apiUrl);
     axios.get(apiUrl).then(displayForecast);
     }
