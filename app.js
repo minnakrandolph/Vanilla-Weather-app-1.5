08f56422f86ab2d7d9c837802f5dfa71f4afb3fe
minna <minnakrandolph@gmail.com>
2023-04-14 07:32:51 -0400

    Update app.js
    for timestamp, changed "date" to now.
    added a call for full year

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 function formatDate(timestamp) {
-    let date = new Date(timestamp);
-    let hours = date.getHours();
+    let now = new Date(timestamp);
+    let hours = now.getHours();
     if (hours < 10)  {
         hours = `0${hours}`;
     }
-    let minutes = date.getMinutes();
+    let minutes = now.getMinutes();
     if(minutes < 10)  {
         minutes = `0${minutes}`;
     }
+    let year = now.getFullYear();
+
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = days[date.getDay()];
+    let day = days[now.getDay()];
     return `${day}${hours}:${minutes}`;
 }
 
