3559562434884e72f09f31c00476c5cb633a5c70
minna <minnakrandolph@gmail.com>
2023-04-17 06:58:38 -0400

    Update app.js
    created new function for forecast.

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ function formatDate(timestamp) {
 function formatDay(timestamp) {
     let date = new Date(timestamp * 1000);
     let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    return day;
+    return days[day];
 }
 
 function displayForecast(response) {
