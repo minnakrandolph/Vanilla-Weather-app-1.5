d2e519c14bb9b950992b5e624a08d844d23e2670
minna <minnakrandolph@gmail.com>
2023-04-18 16:35:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -42,10 +42,10 @@ function displayForecast(response) {
                 />
               <div class="weather-forecast-temperatures">
               <span class="weather-forecast-temperature-max">
-                  ${Math.round(forecastDay.temp.max)}°
+                  ${Math.round(forecastDay.temp.maximum)}°
                 </span>
                 <span class="weather-forecast-temperature-min">
-                 ${Math.round(forecastDay.temp.min)}°
+                 ${Math.round(forecastDay.temp.minimum)}°
                  </span>
                 </div>
               </div>
